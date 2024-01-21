b2b6973b745337e82c58f4f293ce738838e1037e
Tamilkumaran A <tamilkumaranarul007@gmail.com>
2024-01-21 20:09:56 +0530

    v1.0

---
+++ operators.js
@@ -0,0 +1,25 @@
+//Assignment operator
+var a=10
+console.log("Assignment operator output:",a)
+//
+var aA=10
+var bA=5
+console.log("Type of aA:",typeof aA)
+console.log("Arithmetic operator addition output:",aA+bA)
+console.log("Arithmetic operator subtraction output:",aA-bA)
+console.log("Arithmetic operator multiplication output:",aA*bA)
+console.log("Arithmetic operator divison output:",aA/bA)
+console.log("Arithmetic operator remainder output:",aA%bA)
+//logical operator-> &&,||,!
+var aL=10
+var bL=25
+console.log("logical operator AND output",aL&&bL)
+console.log("logical operator || output",aL||bL)
+console.log("logical operator ! output",!aL)
+console.log(10&&20)
+console.log(10&&60)
+console.log(10||20)
+console.log(0||20)
+console.log(0||20)
+console.log(100||20&&0)
+console.log((100||30)&&0)
---
+++ variable.js
@@ -0,0 +1,10 @@
+a='a'
+console.log("This varaiable is general variable:",a)
+var b=10
+console.log("This variable belongs to var type value:",b)
+let c='let'
+console.log("This variable belongs to let type value:",c)
+const d=100
+console.log("This variable belongs to const type value:",d*2)
+b=20
+console.log("This variable belongs to var type value:",b)
